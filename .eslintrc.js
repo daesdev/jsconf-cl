16a9675f5af16d562012fd114de0d5a7a6980bc3
Darío Espinoza <dario@fixu.cl>
2025-06-16 11:50:02 -0400

    feat: :sparkles: Add configuration files for ESLint, Prettier, Stylelint, and VSCode; update package.json with new dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    // Tus reglas personalizadas
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  ],
+};
--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,12 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: "es5",
+  printWidth: 80,
+  endOfLine: "auto",
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
